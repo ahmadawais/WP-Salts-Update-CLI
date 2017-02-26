fde95eab3af7034f1ec19a831b81897297a05306
Ahmad Awais <mrahmadawais@gmail.com>
2017-02-26 20:29:24 +0500

    Version 1.0.0 💯

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # WP-Salts-Update-CLI
+
+`WPSUCLI` downloads new salts from the WP API and replaces them with the ones in your wp-config.php file for every site on your server.
+
+## ⚡️ Installation
+
+Open command line terminal (I prefer iTerm2) and run the following command.
+
+```bash
+sudo wget -qO ptcli https://git.io/vPmNx && sudo chmod +x ./ptcli && sudo install ./ptcli /usr/local/bin/ptcli
+```
+
+This command will perform the following actions:
+
+- Use sudo permissions
+- Use wget to download `WPSUCLI` and rename it to `wpsucli` 
+- Make the `wpsucli` executable
+- Install `wpsucli` inside /usr/local/bin/ folder.
+
+## 🙌 Usage
+
+Just run 'wpsucli' and it will update the salts for every wp-config.php file on your server or PC.
---
+++ wpsucli.sh
@@ -0,0 +1,92 @@
+#! /usr/bin/env bash
+#
+# WP Salts Update CLI.
+#
+# Version 1.0.0
+#
+# WPSUCLI downloads new salts from the WP API and replaces
+# them with the ones in your wp-config.php file.
+#
+
+# Colors.
+#
+# colors from tput
+# http://stackoverflow.com/a/20983251/950111
+# Usage:
+# echo "${redb}red text ${gb}green text${r}"
+bb=`tput setab 0` #set background black
+bf=`tput setaf 0` #set foreground black
+gb=`tput setab 2` # set background green
+gf=`tput setab 2` # set background green
+blb=`tput setab 4` # set background blue
+blf=`tput setaf 4` # set foreground blue
+rb=`tput setab 1` # set background red
+rf=`tput setaf 1` # set foreground red
+wb=`tput setab 7` # set background white
+wf=`tput setaf 7` # set foreground white
+r=`tput sgr0`     # r to defaults
+
+clear
+
+# STARTING.
+echo "—"
+echo "${gb}${bf} WP SALTS UPDATE CLI ⚡️  ${r}"
+echo "${wb}${bf} Version 1.0.0 ${r}"
+echo "${wb}${bf} Automated WP Salts update.${r}"
+echo "—"
+
+# Help.
+	if [[ $@ == "-h" || $@ == "help" || $@ == "--help" ]] ; then
+		echo "——————————————————————————————————"
+		echo "⚡️ Usage: WPSUCLI [ -h | help ]"
+		echo "⚡️  - [ -h | help ] Use help."
+		echo "⚡️  - Just run 'wpsucli' and it will update the salts for every wp-config.php file on your server or PC."
+		echo "——————————————————————————————————"
+		exit 0
+	fi
+
+echo "-"
+echo "--------------- ⏲  LET' START... ---------------"
+echo "-"
+
+# The PWD.
+THE_PWD=$(pwd)
+
+# Path to WordPress installations.
+SITESTORE=./var/www
+
+# Create array of sites based on folder names and ignore '22222' folders of EasyEngine.
+SITELIST=($(ls -lh $SITESTORE | awk '{print $9}'))
+
+# Start the loop.
+find . -name wp-config.php -print | while read line
+do
+	echo "LINE: $line"
+	SITE="$(basename "$(dirname "$line")")"
+
+	# Get dir path where wp-config.php file is present.
+	DIR=$( cd -P "$( dirname "$line" )" && pwd )/
+	# echo "DIR: $DIR"
+	cd $DIR
+
+	echo "-"
+	echo "${wb} ${bf}--------------- ⏲️  UPDATING SALTS FOR: $SITE... ---------------${r}"
+
+	## Download the new salts to file cal salts.
+	curl "https://api.wordpress.org/secret-key/1.1/salt/" -o salts
+
+	echo "${wb} ${bf}--------------- 🎯  SALTS DOWNLOADED... ---------------${r}"
+
+	# Split wp-config.php into 3 on the first and last definition statements.
+	csplit wp-config.php '/AUTH_KEY/' '/NONCE_SALT/+1'
+
+	# Recombine the first part, the new salts and the last part.
+	cat xx00 salts xx02 > wp-config.php
+
+	# Tidy up.
+	rm salts xx00 xx01 xx02
+
+	echo "${gb} ${bf}--------------- ✔︎✔︎✔︎ DONE! SITE: $SITE SALTS UPDATED!!! 💯 🎉 ✔✔✔ ---------------${r}"
+	echo "-"
+  cd $THE_PWD
+done
